c70646169edd579b5ad14317b69e86251b55b1f3
vagrant <vagrant@localhost.localdomain>
2019-10-21 02:28:13 +0000

    Initial commit of the workstation cookbook

---
+++ recipes/setup.rb
@@ -0,0 +1,14 @@
+package 'Install packages' do
+  package_name Array['tree', 'ntp', 'nano', 'git']
+  action :install
+end
+
+file '/etc/motd' do
+  content 'This server is property of Andres Herrera'
+  owner 'root'
+  group 'root'
+end
+
+service 'ntpd' do 
+  action [ :enable, :start]
+end
